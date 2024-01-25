cb8b210bf4bc68ceb5d95976006655ba807eb2c0
israjime <israhjime@gmail.com>
2024-01-24 23:51:55 -0700

    added countNumberOfArrays

--- countNumberOfArrays.js
+++ countNumberOfArrays.js
@@ -1,5 +1,14 @@
 const countNumberOfArrays = function(arr){
-    // Not sure how to do this one.
+    // keeps track of number of arrays
+    var count = 0;
+    
+    for (let i=0 ; i < arr.length ; i++){
+        // checks type of object, if an array, add one to count.
+        if ((Array.isArray(arr[i]))){
+            count += 1;
+        }
+    }
+    return count;
 };
 
 module.exports = countNumberOfArrays;
